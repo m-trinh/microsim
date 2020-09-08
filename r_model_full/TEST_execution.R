a1b38de01b124a5aef1a3d653693406e0b9a901a
clzhang1547 <lzhang1547@gmail.com>
2020-09-08 09:00:36 -0700

    TB merge with LP fix 3.15 (try 2)

--- r_model_full/TEST_execution.R
+++ r_model_full/TEST_execution.R
@@ -5,7 +5,7 @@ options(error=recover)
 meth <- 'Logistic Regression GLM'
 timestart <<- Sys.time()
 d <- policy_simulation(
-                  base_bene_level=.55,
+                  base_bene_level=.6,
                   impute_method = meth,
                   acs_year=2016,
 		              makelog = FALSE,
@@ -14,15 +14,14 @@ d <- policy_simulation(
 		              SELFEMP=FALSE,
 		              place_of_work = TRUE,
 		              dual_receiver = 1,
-		              alpha=1,
+		              alpha=0,
                   ext_resp_len = TRUE, sens_var = 'resp_len', progalt_post_or_pre ='post',
-		              ext_base_effect=TRUE, extend_prob=.01, extend_days=1, extend_prop=1.01, topoff_rate=.01, topoff_minlength=10,
-                  bene_effect=FALSE, full_particip=FALSE, wait_period=5, clone_factor=1, week_bene_cap=1216,
-                  own_uptake=.01, matdis_uptake=.01, bond_uptake=.01, illparent_uptake=.01,
-                  illspouse_uptake=.01, illchild_uptake=.01,
-                  maxlen_own =260, maxlen_matdis =260, maxlen_bond =30, maxlen_illparent =30, 
-                  maxlen_PFL= 30, maxlen_DI=260, maxlen_total=260,
-                  maxlen_illspouse =30, maxlen_illchild =30,earnings=3840, dependent_allow = c(.07,.07,.07,.07,.07), random_seed=NULL)
+		              ext_base_effect=TRUE,
+                  bene_effect=FALSE, full_particip=FALSE, wait_period=5, clone_factor=1, week_bene_cap=804,
+                  own_uptake=.0809, matdis_uptake=.027, bond_uptake=.0102, illparent_uptake=.0009,
+                  illspouse_uptake=.0014, illchild_uptake=.0006,
+                  maxlen_own =150, maxlen_matdis =150, maxlen_bond =30, maxlen_illparent =30, maxlen_illspouse=30, maxlen_illchild=30,
+                  maxlen_PFL= 150, maxlen_DI=150, maxlen_total=150,earnings=3840, dependent_allow = c(.07,.07,.07,.07,.07), random_seed=12345)
 
 timeend <<- Sys.time()
 print(timeend - timestart)
