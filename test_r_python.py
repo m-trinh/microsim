47570276e0458d0d0cc79903d2681e65d7a9aebf
clzhang1547 <lzhang1547@gmail.com>
2020-09-09 16:37:08 -0700

    TB merged with LP fix 3.17

--- test_r_python.py
+++ test_r_python.py
@@ -12,7 +12,7 @@ import numpy as np
 from _5a_aux_functions import *
 ## Read in post-sim ACS
 dp = pd.read_csv('./output/output_20200908_090654_main simulation/acs_sim_ri_20200908_090654.csv')
-dr = pd.read_csv('./output/output_20200908_091148/acs_sim_ri_20200908_091148.csv')
+dr = pd.read_csv('./output/output_20200908_122843/acs_sim_ri_20200908_122843.csv')
 ## Find which persons are in R but not Python
 dm = pd.merge(dp[['SERIALNO', 'SPORDER']], dr[['SERIALNO', 'SPORDER']], how='outer', indicator=True)
 dm = dm[dm['_merge']!='both']
